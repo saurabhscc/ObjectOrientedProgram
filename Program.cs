1121f691ab5e992ae22127abc14d8ead2254668d
saurabhs <saurabhpshirsath@gmail.com>
2021-08-02 12:58:03 +0530

    [saurabh] Add : Json file

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using ObjectOrientedProgram.InventoryManagement;
+using System;
+
+namespace ObjectOrientedProgram
+{
+    class Program
+    {
+        const string InventoryJson = @"C:\Users\saura\source\repos\ObjectOrientedProgram\ObjectOrientedProgram\InventoryManagement\Inventory.json";
+        static void Main(string[] args)
+        {
+            InventoryMain main = new InventoryMain();
+            main.DisplayData(InventoryJson);
+        }
+    }
+}
